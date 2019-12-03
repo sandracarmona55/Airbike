95cb042db629efcd90bce0849e7b5d0b20a9696d
Alexander Hollmann <alexander.hollmann@whu.edu>
2019-12-03 11:32:37 +0100

    Added Controller Bikes

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,16 @@
+class BikesController < ApplicationController
+  def index
+    @bikes = Bike.all
+  end
+
+  def show
+    @bike = Bike.find(params[:id])
+  end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.create(params[:bike])
+end
