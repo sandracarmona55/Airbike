58314d2f68cdffc116360d90aad19397c592d74c
Cassandre Houze <cassandredelaulnoit@gmail.com>
2019-12-04 16:10:54 +0100

    add geocoder and change model of bike

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -8,4 +8,6 @@ class Bike < ApplicationRecord
   validates :photo, presence: true
   validates :category, presence: true
   validates :price, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
