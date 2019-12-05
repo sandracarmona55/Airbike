f1479a87187db398b36024a384c3ef01c264ab4d
Cassandre Houze <cassandredelaulnoit@gmail.com>
2019-12-05 13:49:44 +0100

    finished writting routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :bikes
+  resources :bikes do
+    resources :bookings, only: [:create, :update]
+  end
+  get '/bookings/requests', to: 'bookings#request', as: :request
+  get '/bookings/admin', to: 'bookings#admin', as: :admin
   root to: 'pages#home'
 end
