8956ed80daf821656165c5523e0be2ef8b1169d2
Alexander Hollmann <alexander.hollmann@whu.edu>
2019-12-05 16:12:05 +0100

    added search for map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -34,6 +34,7 @@ const initMapbox = () => {
       .addTo(map);
   });
     fitMapToMarkers(map, markers);
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }));
   }
 };
 
