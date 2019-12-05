76597145de15718059c1369f637d627e7bf107fc
Alexander Hollmann <alexander.hollmann@whu.edu>
2019-12-05 16:17:30 +0100

    added the autocomplete to the create form

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,1 +1,11 @@
+import places from 'places.js';
 
+const initAutocomplete = () => {
+  const addressInput = document.getElementById('bike_address')
+  //grabbing the element of the dom(my input field for address) where i want to insert the autocomplete
+  if (addressInput) {
+    places({ container: addressInput });
+  }
+};
+
+export { initAutocomplete };
