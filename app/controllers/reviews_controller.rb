949948a83bd96cbddd58ab1428e143b908f7dc41
Sandra Carmona <sandracarmona55@gmail.com>
2019-12-06 14:13:53 +0100

    Reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,13 @@
+class ReviewsController < ApplicationController
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+    @review.booking = @booking
+    @review.save
+  end
+end
